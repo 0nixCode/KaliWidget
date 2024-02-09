a28b575436a4897678c2839743a213adbe2f9eb2
onix <31325020+0nixCode@users.noreply.github.com>
2024-02-08 20:58:53 -0500

    ethernet.sh

--- Archivos/bin/ethernet.sh
+++ Archivos/bin/ethernet.sh
@@ -2,12 +2,16 @@
 
 ip="$(/usr/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}')"
 
-if [ "$ip" != "" ]; then
+if [ "${ip}" != "" ]; then
   printf "<icon>network-vpn-symbolic</icon>"
-  printf "<txt> ${ip}</txt>"
-  printf "<tool>Internet</tool>"
+  printf "<txt>${ip}</txt>"
+  if command -v xclip; then
+    printf "<txtclick>sh -c 'printf ${ip} | xclip -selection clipboard'</txtclick>"
+    printf "<tool>VPN IP (click to copy)</tool>"
+  else
+    printf "<tool>VPN IP (install xclip to copy to clipboard)</tool>"
+  fi
 else
-  printf "<icon>network-vpn-acquiring</icon>"
-  printf "<txt> Sin Internet</txt>"
-  printf "<tool>Sin Internet</tool>"
+  printf "<icon>network-vpn-symbolic</icon>"
+  printf "<txt>Sin Internet</txt>"
 fi
