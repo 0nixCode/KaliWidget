369a8a3b587297570b3719add20f61b249823372
onix <31325020+0nixCode@users.noreply.github.com>
2024-02-08 21:00:00 -0500

    target.sh

--- Archivos/bin/target.sh
+++ Archivos/bin/target.sh
@@ -4,11 +4,15 @@ ip_address=$(cat $HOME/.config/bin/target/target.txt | awk '{print $1}')
 machine_name=$(cat $HOME/.config/bin/target/target.txt | awk '{print $2}')
  
 if [ $ip_address ] && [ $machine_name ]; then
-    printf "<icon>w3af</icon>"
-    printf "<txt> $ip_address - $machine_name</txt>"
-    printf "<tool>Objetivo Localizado</tool>"
+    printf "<icon>mark-location</icon>"
+    printf "<txt>$ip_address - $machine_name</txt>"
+  if command -v xclip; then
+    printf "<txtclick>sh -c 'printf ${ip_address} | xclip -selection clipboard'</txtclick>"
+    printf "<tool>VPN IP (click to copy)</tool>"
+  else
+    printf "<tool>VPN IP (install xclip to copy to clipboard)</tool>"
+  fi
 else
-    printf "<icon>w3af</icon>"
-    printf "<txt> Sin Objetivo</txt>"
-    printf "<tool>Sin Objetivo</tool>"
+  printf "<icon>mark-location</icon>"
+  printf "<txt>Sin Objetivo</txt>"
 fi
