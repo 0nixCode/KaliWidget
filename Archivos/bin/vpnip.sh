607a63f87a5eaa4db016769be39a93708da05d8b
onix <31325020+0nixCode@users.noreply.github.com>
2024-02-08 21:00:24 -0500

    vpnip.sh

--- Archivos/bin/vpnip.sh
+++ Archivos/bin/vpnip.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-ip="$(ip a s tun0 2>/dev/null | grep -o -P '(?<=inet )[0-9]{1,3}(\.[0-9]{1,3}){3}')"
+interface="$(ip tuntap show | cut -d : -f1 | head -n 1)"
+ip="$(ip a s "${interface}" 2>/dev/null \
+        | grep -o -P '(?<=inet )[0-9]{1,3}(\.[0-9]{1,3}){3}')"
 
-if [ "$ip" != "" ]; then
+if [ "${ip}" != "" ]; then
   printf "<icon>draw-cuboid</icon>"
-  printf "<txt> ${ip}</txt>"
-  printf "<tool>VPN IP</tool>"
+  printf "<txt>${ip}</txt>"
+  if command -v xclip; then
+    printf "<txtclick>sh -c 'printf ${ip} | xclip -selection clipboard'</txtclick>"
+    printf "<tool>VPN IP (click to copy)</tool>"
+  else
+    printf "<tool>VPN IP (install xclip to copy to clipboard)</tool>"
+  fi
 else
   printf "<icon>draw-cuboid</icon>"
-  printf "<txt> Desconectado</txt>"
-  printf "<tool>SIN VPN</tool>"
+  printf "<txt>Desconectado</txt>"
 fi
